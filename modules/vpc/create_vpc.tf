9db127cc769b76dffc0b229882b897bd2d6e10b2
Juhász Szabolcs <juhaszszabolcs90@gmail.com>
2023-10-20 18:38:56 +0200

    Create a 2nd Route Table

--- modules/vpc/create_vpc.tf
+++ modules/vpc/create_vpc.tf
@@ -49,11 +49,25 @@ variable "azs" {
  default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
 }
 
-# 3. Internet Gateway
+# 3.Create Internet Gateway
 resource "aws_internet_gateway" "gw" {
  vpc_id = aws_vpc.main.id
 
  tags = {
    Name = "Project VPC IG"
+ }
+}
+
+# 4. Create a 2nd Route Table
+resource "aws_route_table" "second_rt" {
+ vpc_id = aws_vpc.main.id
+
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.gw.id
+ }
+
+ tags = {
+   Name = "2nd Route Table"
  }
 }
